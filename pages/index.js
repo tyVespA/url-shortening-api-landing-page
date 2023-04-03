828ca23909a4b241c049393b53d8e2b15eb66aa0
tyVespA <migliavacca.marco08@gmail.com>
2023-04-03 11:46:36 +0200

    Add Navbar, remove button border

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
       <main className={styles.main}>MAIN</main>
     </>
   );

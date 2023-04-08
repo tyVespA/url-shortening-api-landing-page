9dff22c47812d8b08068b98b7fff0a52e0516493
tyVespA <migliavacca.marco08@gmail.com>
2023-04-08 16:52:33 +0200

    Fix button width and size

--- components/FooterCTA.js
+++ components/FooterCTA.js
@@ -6,7 +6,12 @@ export default function FooterCTA() {
   return (
     <div className={styles.footerCTA}>
       <h2>Boost your links today</h2>
-      <Button backgroundColorValue="cyan" borderRadiusType="rounded">
+      <Button
+        backgroundColorValue="cyan"
+        borderRadiusType="rounded"
+        fontSize="1.2rem"
+        paddingInline="35px"
+      >
         Get Started
       </Button>
     </div>

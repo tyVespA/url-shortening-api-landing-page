2193e1a97ea6060eae8fe17ef6509671b3806a6c
tyVespA <migliavacca.marco08@gmail.com>
2023-04-08 17:19:25 +0200

    Create function to copy the shortened link, style the input field

--- components/ShortenedLink.js
+++ components/ShortenedLink.js
@@ -1,14 +1,38 @@
 import React from "react";
 import Button from "./Button";
 import styles from "../styles/ShortenedLink.module.css";
+import { useRef } from "react";
 
-export default function ShortenedLink({ fullLink, shortenedLink }) {
+export default function ShortenedLink({ fullLink, shortenedLink = "test" }) {
+  const linkRef = useRef(null);
+
+  function copyToClipBoard() {
+    const link = linkRef.current;
+
+    link.select();
+    link.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(link.value);
+    console.log("copied");
+  }
   return (
     <div className={styles.shortenedLinkContainer}>
       <p>Full link: {fullLink}</p>
       <div>
-        <p>Shortened Link: {shortenedLink}</p>
-        <Button backgroundColorValue="cyan">Copy</Button>
+        <p>
+          Shortened Link:{" "}
+          <input
+            className={styles.shortenedLink}
+            type="text"
+            value={shortenedLink}
+            ref={linkRef}
+            size={shortenedLink.length}
+            readOnly
+            disabled
+          />
+        </p>
+        <div className={styles.buttonContainer} onClick={copyToClipBoard}>
+          <Button backgroundColorValue="cyan">Copy</Button>
+        </div>
       </div>
     </div>
   );
